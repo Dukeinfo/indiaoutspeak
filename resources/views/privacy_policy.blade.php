02d5794c9214ee782ae2dba3196e42f82da15889
Amankhalsa <amansin31@gmail.com>
2024-04-04 08:59:36 +0530

    [ade added

--- resources/views/privacy_policy.blade.php
+++ resources/views/privacy_policy.blade.php
@@ -22,38 +22,16 @@
                                     Privacy-policy
                                 </h3>
                                   <p class="f1-s-13 cl6 p-b-25">
-                                    Digital media, as defined in a document that lays out the government’s framework to regulate online content, will cover digitised content that can be transmitted over the internet or computer networks.<br><br>
-                                    Digital media includes intermediaries such as Twitter and Facebook, and publishers of news and current affairs content. It also includes so-called curators of such content.<br><br>
-                                    <b>Govt rules for online media to include takedown power</b><br>
-                                    Digital media, as defined in a document that lays out the government’s framework to regulate online content, will cover digitised content that can be transmitted over the internet or computer networks.
-                                    The stategovernment wants to treat online news media publishers at par with traditional media publishers, such as newspapers and news channels, and also bring them under the ambit of section 69(A) of the Information Technology Act that gives takedown powers to the government, according to new guidelines yet to be put in force.<br><br>
-            
-                                    Digital media, as defined in a document that lays out the government’s framework to regulate online content, will cover digitised content that can be transmitted over the internet or computer networks. It includes intermediaries such as Twitter and Facebook, and publishers of news and current affairs content. It also includes so-called curators of such content.<br><br>
-                                    So far, online news media has not been unregulated, with the information & broadcasting ministry brining it under its ambit last year, but not yet formalizing rules for it. Nor are intermediaries, especially social media companies that take refuge in their intermediary status when it comes to owing up responsibility for content. According to the guidelines titled Information Technology (Guidelines for intermediaries and digital media ethics code) Rules, 2021, digital news media publishers will need to follow rules that apply to print and electronic media.<br><br>
-                                    We rely on legitimate business interests as the legal basis for such processing and the legitimate interest is the analyzing the use of the Services and improving the same;<br>
-                                    (v) To send you Information, promotions and updates including marketing and advertising materials in relation to our Services and those of third party organizations selected by us. We will only do this where we reasonably believe that our Services may be of interest to you and you consent to being provided with such information. We rely on legitimate business interest as the legal basis to promote our Services to you as the legal basis for this processing;<br>
-                                    (vi) To prevent fraud or other potentially illegal activities (including copyright infringement) and to block disruptive users and protect the safety of users of our Services. The legal basis for this processing is our legitimate business interests being the proper protection of our business against risks and<br>
-                                    (viii) To enforce our terms of Service. The legal basis for this processing being the protection and assertion of our legal rights and the legal rights of others.<br><br>
-                                    In any case, if you would prefer that we do not use your Information to market or promote our products and Services to you, please either (i) tick the relevant box on the form through which we collect your Information (for example, the registration form); (ii) unsubscribe from our electronic communications using the method indicated in the relevant communication; or (iii) contact us at the contact details set out below:<br><br>
-            
-                        
-                                    All our content developed under the roof of indiaoutspeakOffice ,not claim of copy to other part.<br>
-                                    Name of Owner Parminder Singh Jatpuri<br>
-                                    SCO 86-87- Sector 45 Chandigarh<br>
-                                    Contact No 9815481679<br>
-                                    Studio Address SCO 64 Phase ii Mohali<br>
-                                    Email ID psjatpuri@gmail.com<br>
-                                    www.indiaoutspeak.com which is empanel by the Government   of Punjab (INDIA)<br>
-                                     
-                                    •	Proof of Reading and Verified by the Editor<br>
-                                    •	3 year date on server rather the backup from opening to current.<br>
-                                    •	Language English Hindi Punjabi<br>
-                                    •	Advertisement space as top Banner, Top Middle Banner, Top Bottom Banner in Every page<br>
-                                    •	Display Add in Each News.<br>
-                                    News apps that aggregate content from different publishing sources must be transparent about the publishing source of the content in the app and each of the sources must meet News policy requirements.
-                                    We Declare that no copied matter from other sources app and news Website , No Other unsocial Activity , Abusive Language , Video will displayed.<br><br>
-                                    All legal disputes are subject to under Mohali Court Jurisdiction.
-            
+                                    Curabitur volutpat bibendum molestie. Vestibulum ornare gravida semper. Aliquam a dui suscipit, fringilla metus id, maximus leo. Vivamus sapien arcu, mollis eu pharetra vitae, condimentum in orci. Integer eu sodales dolor. Maecenas elementum arcu eu convallis rhoncus. Donec tortor sapien, euismod a faucibus eget, porttitor quis libero.
+                                 
+                                  </p> 
+                                    <p class="f1-s-13 cl6 p-b-25">
+                                    
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet est vel orci luctus sollicitudin. Duis eleifend vestibulum justo, varius semper lacus condimentum dictum. Donec pulvinar a magna ut malesuada. In posuere felis diam, vel sodales metus accumsan in. Duis viverra dui eu pharetra pellentesque. Donec a eros leo. Quisque sed ligula vitae lorem efficitur faucibus. Praesent sit amet imperdiet ante. Nulla id tellus auctor, dictum libero a, malesuada nisi. Nulla in porta nibh, id vestibulum ipsum. Praesent dapibus tempus erat quis aliquet. Donec ac purus id sapien condimentum feugiat.
+                                  </p>
+                                  <p class="f1-s-13 cl6 p-b-25">
+
+                                    Praesent vel mi bibendum, finibus leo ac, condimentum arcu. Pellentesque sem ex, tristique sit amet suscipit in, mattis imperdiet enim. Integer tempus justo nec velit fringilla, eget eleifend neque blandit. Sed tempor magna sed congue auctor. Mauris eu turpis eget tortor ultricies elementum. Phasellus vel placerat orci, a venenatis justo. Phasellus faucibus venenatis nisl vitae vestibulum. Praesent id nibh arcu. Vivamus sagittis accumsan felis, quis vulputate
                                 </p>
                              
 
