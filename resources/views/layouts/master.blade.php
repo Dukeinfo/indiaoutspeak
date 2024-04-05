d0c266f2dc387af22f736e87ecd02006721bdbe9
Amankhalsa <amansin31@gmail.com>
2024-04-06 01:48:40 +0530

    public

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -24,14 +24,14 @@
     <meta name="msapplication-TileColor" content="#ffc40d">
     <meta name="theme-color" content="#ffffff">
     <link rel="icon" type="image/png" href="{{asset('assets/images/icons/favicon.png')}}" />
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/fonts/fontawesome-5.0.8/css/fontawesome-all.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/fonts/iconic/css/material-design-iconic-font.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/vendor/animate/animate.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/vendor/css-hamburgers/hamburgers.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/vendor/animsition/css/animsition.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/util.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/main.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('/publicassets/vendor/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('/publicassets/fonts/fontawesome-5.0.8/css/fontawesome-all.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('/publicassets/fonts/iconic/css/material-design-iconic-font.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('/publicassets/vendor/animate/animate.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('/publicassets/vendor/css-hamburgers/hamburgers.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('/publicassets/vendor/animsition/css/animsition.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('/publicassets/css/util.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('/publicassets/css/main.css')}}">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     @livewireStyles
 
